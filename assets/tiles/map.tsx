f9d79844cf19e1e14f1ad8c3f0483adb1184a314
libredesarrollo <desarrollolibre.net@gmail.com>
2023-02-03 07:24:08 -0400

    mapa

--- assets/tiles/map.tsx
+++ assets/tiles/map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="tile" tilewidth="48" tileheight="48" tilecount="855" columns="19">
- <image source="../images/tile.png" width="912" height="2160"/>
+ <image source="tile.png" width="912" height="2160"/>
 </tileset>
